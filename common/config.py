d75024deb1f199a4be1a4364e7d88a6a6afbe2aa
Nishadrin <nishadrin@gmail.com>
2020-03-14 15:39:47 +0300

    add to config settings: path to save db and add init file

--- common/config.py
+++ common/config.py
@@ -1,3 +1,4 @@
+# link
 MAIN_LINK = 'https://geekbrains.ru/'
 COURSES_LINK_PREFIX = 'education/'
 WEBINAR_TYPE_PREFIX = 'lessons/'
@@ -5,3 +6,6 @@ VIDEO_TYPE_PREFIX = 'chapters/'
 INTERACTIVE_TYPE_PREFIX = 'study_groups/'
 WEBINAR_HOMEWORK_PREFIX = 'homework/'
 INTERACTIVE_HOMEWORK_PREFIX = 'homeworks/'
+
+# database
+DATABASE_PATH = 'sqlite:///storage_gb.db3'
