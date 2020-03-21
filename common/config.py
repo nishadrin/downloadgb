26d942a5992ad3764eda722104e334add8d96940
Nishadrin <nishadrin@gmail.com>
2020-03-21 15:28:17 +0300

    add to config file

--- common/config.py
+++ common/config.py
@@ -2,14 +2,27 @@ from logging import DEBUG
 
 ENCODING = 'utf-8'
 
-# link
-MAIN_LINK = 'https://geekbrains.ru/'
-COURSES_LINK_PREFIX = 'education/'
-WEBINAR_TYPE_PREFIX = 'lessons/'
-VIDEO_TYPE_PREFIX = 'chapters/'
-INTERACTIVE_TYPE_PREFIX = 'study_groups/'
-WEBINAR_HOMEWORK_PREFIX = 'homework/'
-INTERACTIVE_HOMEWORK_PREFIX = 'homeworks/'
+# links
+MAIN_LINK = 'https://geekbrains.ru'
+COURSES_LINK_PREFIX = '/education'
+WEBINAR_TYPE_PREFIX = '/lessons'
+VIDEO_TYPE_PREFIX = '/chapters'
+INTERACTIVE_TYPE_PREFIX = '/study_groups'
+WEBINAR_HOMEWORK_PREFIX = '/homework'
+INTERACTIVE_HOMEWORK_PREFIX = '/homeworks'
+INTERACTIVE_VIDEOS_PREFIX = '/videos'
+
+# type
+WEBINAR_TYPE = 'lesson'
+VIDEO_TYPE = 'chapter'
+INTERACTIVE_TYPE = 'study_group'
+# types
+WEBINARS_TYPE = 'lessons'
+VIDEOS_TYPE = 'chapters'
+# gb type on gb
+GB_WEBINAR_TYPE = 'basic'
+GB_VIDEO_TYPE = 'video'
+GB_INTERACTIVE_TYPE = 'interactive'
 
 # database
 DATABASE_PATH = 'sqlite:///storage_gb.db3'
@@ -18,3 +31,6 @@ DATABASE_PATH = 'sqlite:///storage_gb.db3'
 LOGGING_LEVEL = DEBUG
 LOGGING_FORMAT = '%(asctime)s - %(levelname)-8s - %(module)s - %(message)s'
 LOG_FILE_NAME = 'logs.log'
+
+# not completed courses
+STATES_NOT_COMPLETED_COURSES = ('begin', 'continue', 'require_courses')
